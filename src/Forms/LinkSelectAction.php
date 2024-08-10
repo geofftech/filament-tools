5d88f85c46c47eeeadb909280763280fc7bfff12
Geoff <geoff@geofftech.com>
2024-08-10 20:51:02 +1000

    pint

--- src/Forms/LinkSelectAction.php
+++ src/Forms/LinkSelectAction.php
@@ -8,10 +8,10 @@ class LinkSelectAction
 {
     public static function make($name, $linkForm): Action
     {
-        return Action::make($name.'_link')
+        return Action::make($name . '_link')
             ->iconButton()
             ->icon('heroicon-m-arrow-top-right-on-square')
-            ->url(fn ($get) => $linkForm::getUrl(['record' => $get($name)]))
-            ->visible(fn ($get) => (bool) $get($name));
+            ->url(fn($get) => $linkForm::getUrl(['record' => $get($name)]))
+            ->visible(fn($get) => (bool) $get($name));
     }
 }
