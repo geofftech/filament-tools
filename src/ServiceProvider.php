f21bf4587b11db1a3ba93133a010fd39d9c70041
Geoff <geoff@geofftech.com>
2024-09-24 10:52:58 +1000

    add service provider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace GeoffTech\FilamentTools;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    public function register(): void
+    {
+    }
+
+    public function boot(): void
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'filament-tools');
+    }
+}
