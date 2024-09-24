3f8651e709bc2c27e719f9f27d1f89a7f4c44ce9
Geoff <geoff@geofftech.com>
2024-09-25 08:40:14 +1000

    set max length for text input to 255 globally

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,16 +2,17 @@
 
 namespace GeoffTech\FilamentTools;
 
+use Filament\Forms\Components\TextInput;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
 {
-    public function register(): void
-    {
-    }
+    public function register(): void {}
 
     public function boot(): void
     {
+        TextInput::configureUsing(modifyUsing: fn($c) => $c->maxLength(255));
+
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'filament-tools');
     }
 }
