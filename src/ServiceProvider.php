cb88895c6a01ba0299187b485c7b23ac1d852b53
Geoff <geoff@geofftech.com>
2024-09-28 17:49:43 +1000

    set text max length only if not numeric

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace GeoffTech\FilamentTools;
 
+use Filament\Forms\Components\Component;
 use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\TextInput;
@@ -9,15 +10,23 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
 {
-    public function register(): void
-    {
-    }
+    public function register(): void {}
 
     public function boot(): void
     {
-        TextInput::configureUsing(modifyUsing: fn ($c) => $c->maxLength(255));
-        RichEditor::configureUsing(modifyUsing: fn ($c) => $c->disableToolbarButtons(['attachFiles']));
-        MarkdownEditor::configureUsing(modifyUsing: fn ($c) => $c->disableToolbarButtons(['attachFiles']));
+        TextInput::configureUsing(
+            modifyUsing: fn($c) => $c->maxLength(
+                fn(Component $component) => $component->isNumeric()
+                    ? null
+                    : 255
+            )
+        );
+        RichEditor::configureUsing(
+            modifyUsing: fn($c) => $c->disableToolbarButtons(['attachFiles'])
+        );
+        MarkdownEditor::configureUsing(
+            modifyUsing: fn($c) => $c->disableToolbarButtons(['attachFiles'])
+        );
 
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'filament-tools');
     }
