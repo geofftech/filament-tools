f5e616c0e6b749d022711304c545d0b706cab167
Geoff <geoff@geofftech.com>
2025-07-09 14:58:30 +1000

    temp fix: disableToolbarButtons broken

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,10 +2,10 @@
 
 namespace GeoffTech\FilamentTools;
 
-use Filament\Forms\Components\Component;
 use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Component;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
@@ -21,12 +21,16 @@ class ServiceProvider extends BaseServiceProvider
                     : 255
             )
         );
-        RichEditor::configureUsing(
-            modifyUsing: fn($c) => $c->disableToolbarButtons(['attachFiles'])
-        );
-        MarkdownEditor::configureUsing(
-            modifyUsing: fn($c) => $c->disableToolbarButtons(['attachFiles'])
-        );
+
+        // ! temp fix: disableToolbarButtons broken
+        // RichEditor::configureUsing(
+        //     modifyUsing: fn($c) => $c->disableToolbarButtons(['attachFiles'])
+        // );
+
+        // ! temp fix: disableToolbarButtons broken
+        // MarkdownEditor::configureUsing(
+        //     modifyUsing: fn($c) => $c->disableToolbarButtons(['attachFiles'])
+        // );
 
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'filament-tools');
     }
